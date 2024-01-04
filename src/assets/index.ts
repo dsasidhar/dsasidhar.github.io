d791c305f4fd6028ab0d1fd4529b52b7d834218c
Sasidhar Dwadasi <sasidhar.dwadasi@gmail.com>
2024-01-04 15:37:28 +0530

    small fix

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -7,7 +7,6 @@ import mobileDev from "./mobileDev.png";
 import multiMonitor from "./multiMonitor.jpg";
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import graphql from "./tech/graphql.png";
 import html from "./tech/html.png";
@@ -16,7 +15,6 @@ import nodejs from "./tech/nodejs.png";
 import postgresql from "./tech/postgresql.png";
 import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
-import rubyrails from "./tech/rubyrails.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 
